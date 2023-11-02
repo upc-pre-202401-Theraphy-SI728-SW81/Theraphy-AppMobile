09c5f62fc08c21289ddf6af201e856b06860f281
GabrielGomezDlc <89089619+GabrielGomezDlc@users.noreply.github.com>
2023-11-02 09:33:20 -0500

    feat(patients-list): created patients-list.dart

---
+++ lib/data/model/therapy.dart
@@ -0,0 +1,40 @@
+import 'package:mobile_app_theraphy/data/model/patient.dart';
+import 'package:mobile_app_theraphy/data/model/physiotherapist.dart';
+
+class Therapy {
+  int id;
+  String therapyName;
+  String description;
+  String appointmentQuantity;
+  String startAt;
+  String finishAt;
+  bool finished;
+  Patient patient;
+  Physiotherapist physiotherapist;
+
+  Therapy(
+      {required this.id,
+      required this.therapyName,
+      required this.description,
+      required this.appointmentQuantity,
+      required this.startAt,
+      required this.finishAt,
+      required this.finished,
+      required this.patient,
+      required this.physiotherapist
+      });
+
+  Therapy.fromJson(Map<String, dynamic> json)
+      : this(
+            id: json['id'],
+            therapyName: json['therapyName'],
+            description: json['description'],
+            appointmentQuantity: json['appointmentQuantity'],
+            startAt: json['startAt'],
+            finishAt: json['finishAt'],
+            finished: json['finished'],
+            patient: Patient.fromJson(json['patient']),
+            physiotherapist: Physiotherapist.fromJson(json['physiotherapist'])
+            );
+           
+}
