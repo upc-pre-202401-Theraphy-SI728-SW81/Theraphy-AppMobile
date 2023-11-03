6879e15848de39054132924708a94b443601e445
delca <u202020239@upc.edu.pe>
2023-11-03 14:32:17 -0500

    fix: Upload Video

--- lib/data/remote/upload_Into_Firebase.dart
+++ lib/data/remote/upload_Into_Firebase.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
+import 'package:path/path.dart';
 
 final FirebaseStorage storage = FirebaseStorage.instance;
 
@@ -26,18 +28,26 @@ Future<String?> uploadImage(File image) async {
 
 Future<String?> uploadVideo(File video) async {
   try {
-    // Obtener la referencia al almacenamiento de Firebase
-    String fileName = DateTime.now().millisecondsSinceEpoch.toString();
+    final storage = FirebaseStorage.instance;
+    String fileName = basename(video.path);
     Reference storageReference = storage.ref().child('videos/$fileName');
 
-    // Subir el video al almacenamiento de Firebase
-    TaskSnapshot uploadTask = await storageReference.putFile(video);
-    String videoUrl = await uploadTask.ref.getDownloadURL();
+    // Convertir el video a formato MP4 con calidad y bitrate especificados
+    final flutterFFmpeg = FlutterFFmpeg();
+    final outputVideoPath = video.path.replaceAll(RegExp(r'\.\w+$'), '.mp4');
+    final cmd = '-i ${video.path} -c:v libx264 -b:v 1000k -c:a aac -b:a 128k $outputVideoPath';
+    await flutterFFmpeg.execute(cmd);
+
+    // Subir el video convertido al almacenamiento de Firebase
+    UploadTask uploadTask = storageReference.putFile(File(outputVideoPath));
+    await uploadTask;
+
+    // Obtener la URL de descarga del video en formato MP4
+    String videoUrl = await storageReference.getDownloadURL();
 
     // Retornar la URL del video
     return videoUrl;
   } catch (e) {
-    // Si ocurre un error, mostrar el mensaje de error y retornar null
     print('Error al cargar el video: $e');
     return null;
   }
